8a503a36393468124a520c45458bbbb1e5071c16
Clemens Stift <clemens@stift.online>
2018-12-08 13:58:30 +0100

    Fixed backend not connecting to DB bug.

---
+++ JaveEEBackend/src/main/java/at/technikumwien/customer/NewsWebService.java
@@ -0,0 +1,16 @@
+package at.technikumwien.customer;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.jws.WebService;
+
+@WebService
+public class NewsWebService {
+	@Inject
+	private NewsService newsService;
+	
+	public List<News> getAllNews() {
+		return newsService.getAllNews();
+	}
+}
